6298449cf7b744d6ab93533e0c26143a6ad757c5
Rohail Kabani <rohail.kabani@gmail.com>
2019-02-17 17:33:00 -0500

    add some more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,8 +5,12 @@ alias gb='git branch '
 alias gc='git commit'
 alias gd='git diff'
 alias go='git checkout '
+alias grm='git fetch origin master && git rebase origin/master'
+alias gp='git push '
+alias gpf='git push --force-with-lease'
 alias gk='gitk --all&'
 alias gx='gitx --all'
+alias g='git'
 
 # Misspelled git
 alias got='git '
