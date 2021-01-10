86009707f6271600dde58165437a13420321ee94
rkabani19 <rohail.kabani@gmail.com>
2021-01-10 15:06:31 -0500

    ammend order of scripts

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # nvim
+mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+rm ~/.config/coc/extensions/package.json
+rm ~/.config/nvim/coc-settings.json
 ln -sf ~/dotfiles/nvim/coc/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sf ~/dotfiles/nvim/coc/package.json ~/.config/coc/extensions/package.json
 
 # zshrc
+rm ~/.zshrc
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 # tmux
+rm ~/.tmux.conf
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # git
 ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
-
--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -3,5 +3,12 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# move iterm profile
+cp ~/dotfiles/terminal/Profile.json ~/Library/ApplicationSupport/iTerm2/DynamicProfiles/
+
+# install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # install pure
 npm install --global pure-prompt
